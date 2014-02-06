8444d9f023840b59275c85a7a58216a12f7235ba
Nathan Jones <thenathanjones@gmail.com>
2014-02-06 08:23:53 +0800

    Revise down the low pass filter so we respond slower.

--- IBeaconLibrary/src/main/java/com/thenathanjones/ibeaconlibrary/services/IBeaconConstants.java
+++ IBeaconLibrary/src/main/java/com/thenathanjones/ibeaconlibrary/services/IBeaconConstants.java
@@ -13,6 +13,6 @@ public class IBeaconConstants {
     public static final int MINOR_INDEX = 27;
     public static final int TXPOWER_INDEX = 29;
     public static final List<Integer> IBEACON_HEADER = Arrays.asList(0x4c, 0x00, 0x02, 0x15);
-    public static final double BLEND_FACTOR = 0.3;
+    public static final double BLEND_FACTOR = 0.1;
     final static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 }
